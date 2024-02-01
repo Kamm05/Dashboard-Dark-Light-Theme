a486746ce31e6d1d87ea3dc5d008df1ddd41fd93
Kamalesh1256 <116736001+Kamm05@users.noreply.github.com>
2024-02-01 14:07:39 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Orders =[
+    {
+productName: 'Foldable Mini Drone',
+productNumber: '85631',
+paymentStatus:'Due',
+shipping:'Pending'
+},
+{
+    productName: ' Drone',
+    productNumber: '85631',
+    paymentStatus:'Due',
+    shipping:'Pending'
+    },
+    {
+        productName: 'Computer',
+        productNumber: '85631',
+        paymentStatus:'Due',
+        shipping:'Pending'
+        },
+        {
+            productName: 'Mouse',
+            productNumber: '85631',
+            paymentStatus:'Due',
+            shipping:'Pending'
+            },
+            {
+                productName: 'GoPro',
+                productNumber: '85631',
+                paymentStatus:'Due',
+                shipping:'Pending'
+                },
+                {
+                    productName: 'Bottles',
+                    productNumber: '85631',
+                    paymentStatus:'Due',
+                    shipping:'Pending'
+                    },
+                    {
+                        productName: 'Iphone',
+                        productNumber: '85631',
+                        paymentStatus:'Due',
+                        shipping:'Pending'
+                        },
+                        {
+                            productName: 'Airpods',
+                            productNumber: '85631',
+                            paymentStatus:'Due',
+                            shipping:'Pending'
+                            },
+                        ]
